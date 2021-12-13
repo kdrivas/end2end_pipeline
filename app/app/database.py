c1ef9be3541fee68fa078361d43613fbe388d069
kdrivas <k.rivas@pucp.edu.pe>
2021-12-12 19:50:29 -0500

    adding db connections

---
+++ app/app/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import os
+
+DATABASE_CONN = os.getenv('DATABASE_CONN', '')
+
+engine = create_engine(DATABASE_CONN)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
